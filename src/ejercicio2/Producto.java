c9deb565f535bf3ee536f7f2dedede5febd4100c
Francisco Garcia Moretta <franciscogarciaa.5a@gmail.com>
2026-09-10 19:05:56 -0300

    Clase producto

---
+++ src/ejercicio2/Producto.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Admin
+ */
+public class Producto {
+
+    private int stock;
+    private int precio;
+    private int id_prod;
+    private String descripcion;
+    private String rubro;
+
+    public Producto(int stock, int precio, int id_prod, String descripcion, String rubro) {
+        this.stock = stock;
+        this.precio = precio;
+        this.id_prod = id_prod;
+        this.descripcion = descripcion;
+        this.rubro = rubro;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getId_prod() {
+        return id_prod;
+    }
+
+    public void setId_prod(int id_prod) {
+        this.id_prod = id_prod;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Producto otro = (Producto) obj;
+        return this.id_prod == otro.id_prod;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_prod);
+    }
+
+    
+}
